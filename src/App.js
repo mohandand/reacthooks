cdf566fc20f1ff0d37b449082a497e1095ff2d68
Mohan Dandigam <mohandand@gmail.com>
2024-09-15 12:27:59 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
-import {useReducer} from 'react';
+import  React, {useReducer} from 'react'
 
-function App() {
+const App = () => {
 
   const reducer = (state, action) => {
-    switch(action.type){
-      case 'INCREMENT':
-        return {count: state.count+1, showText: state.showText}
-      case 'toggleShowText':
-        return {count: state.count, showText: !state.showText}
-      default:
-        return state;
-    }
+      switch(action.type){
+        case 'INCREMENT':
+          return { count: state.count+1, showText: state.showText};
+        case 'toggleShowText':
+          return {count: state.count, showText: !state.showText};
+        default: 
+          return state;
+      }
   }
 
   const [state, dispatch] = useReducer(reducer, {count: 0, showText: true})
 
-
-  return (
-    <div className="App">
+  return(
+    <div>
       <div>Count: {state.count}</div>
-      {state.showText &&  <div>I am visible now</div>}
-      <button onClick= {() => dispatch({type: 'INCREMENT'})}>Count</button>
-      <button onClick= {() => dispatch({type: 'toggleShowText'})}>Toggle Text</button>
+      {state.showText && <div>I am Visible now !!!</div>}
+      <button onClick={() => dispatch({type: 'INCREMENT'})}>Count Increase</button>
+      <button onClick={() => dispatch({type:'toggleShowText' })}>Toggle Text</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
