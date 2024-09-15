b69d6aee895a28ee0ca3be72aeb6748e7203dd0c
Mohan Dandigam <mohandand@gmail.com>
2024-09-15 12:15:32 -0700

    useReducer hook implementtion is completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {useReducer} from 'react';
 
 function App() {
+
+  const reducer = (state, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return {count: state.count+1, showText: state.showText}
+      case 'toggleShowText':
+        return {count: state.count, showText: !state.showText}
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, {count: 0, showText: true})
+
+
   return (
     <div className="App">
-     Hello
+      <div>Count: {state.count}</div>
+      {state.showText &&  <div>I am visible now</div>}
+      <button onClick= {() => dispatch({type: 'INCREMENT'})}>Count</button>
+      <button onClick= {() => dispatch({type: 'toggleShowText'})}>Toggle Text</button>
     </div>
   );
 }
